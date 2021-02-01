72da91d9ec56fb80e1528520e74a04ec3a4d2274
Andrey Tkachenko <andreytkachenko64@gmail.com>
2021-02-01 17:45:44 +0400

    Update ort.h

--- cbits/ort.h
+++ cbits/ort.h
@@ -1,4 +1,4 @@
-#include <onnxruntime/core/session/onnxruntime_c_api.h>
+#include <onnxruntime_c_api.h>
 
 ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_CPU, _In_ OrtSessionOptions* options, int use_arena) ORT_ALL_ARGS_NONNULL;
 ORT_API_STATUS(OrtSessionOptionsAppendExecutionProvider_Tensorrt, _In_ OrtSessionOptions* options, int device_id);
